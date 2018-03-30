47482f8ae7feb06f4c549dc6d947f2ff73c0642b
Niels Hansen <hansenviking@hotmail.com>
2018-03-30 09:46:07 -0500

    Added the ability to jump on it.

---
+++ EECS448 Project 3/Assets/PlayerMoveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+
+    public int playerJumpPower = 375;
+
+	// Update is called once per frame
+	void Update () {
+        PlayerMove();
+	}
+
+    void PlayerMove()
+    {
+        //CONTROLS
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        //PHYSICS
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce (Vector2.up * playerJumpPower);
+    }
+}
