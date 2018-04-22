4665579d441151bb9d6807b8360f699745ffc0a5
Ron Huff <ron.huff@ku.edu>
2018-04-22 18:21:09 -0500

    Add Resume button functionality.

---
+++ EECS448 Project 3/Assets/PauseMenu.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * @file - PauseMenu
+ * @author - Ron Huff
+ * @date - 
+ * @brief - 
+ */
+
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    #region Variables
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+
+	#endregion
+
+	#region Unity Methods
+
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+	}
+
+    #endregion
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void LoadGameMenu()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+
+    }
+	
+}
