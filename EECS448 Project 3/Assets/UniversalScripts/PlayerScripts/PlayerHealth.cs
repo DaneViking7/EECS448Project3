bfc2631db26c38e323fecec8efd007b44289479e
Niels Hansen <hansenviking@hotmail.com>
2018-04-26 14:21:16 -0500

    Added fireball animations and lives system.

--- EECS448 Project 3/Assets/UniversalScripts/PlayerScripts/PlayerHealth.cs
+++ EECS448 Project 3/Assets/UniversalScripts/PlayerScripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
@@ -8,20 +9,25 @@ public class PlayerHealth : MonoBehaviour
 
 	private bool dead; //!< boolean to indicate whether or not player died
     public GameObject canvas; //!< object to display number of coins on the screen
+	public GameObject livesCountUI;
+	private int lives;
+	bool invincible;
     // Use this for initialization
 	void Start()
     {
         dead = false;
+		lives = 3;
+		invincible = false;
     }
 
     // Update is called once per frame
 	void Update()
     {
+		livesCountUI.gameObject.GetComponent<Text>().text = ("Lives: " + lives);
+		if (lives == 0)
+			dead = true;
         if (dead == true)
-        {
             Die();
-        }
-
     }
 
 	void Die()
@@ -33,7 +39,9 @@ public class PlayerHealth : MonoBehaviour
 
 	private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Kill Object")
-            dead = true;
+		if (col.gameObject.tag == "Kill Object")
+			lives--;
+		if (col.gameObject.name == "Heart")
+			lives++;
     }
 }
