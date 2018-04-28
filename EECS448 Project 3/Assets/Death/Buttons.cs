1d85c5dd909c9b70aeb74547ab720c5f470967e1
Annika Kuhnke <akkuhnke@ku.edu>
2018-04-28 13:00:22 -0500

    Victory and death screens completed

---
+++ EECS448 Project 3/Assets/Death/Buttons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	public void LoadMainMenu(){
+		SceneManager.LoadScene ("Menu", LoadSceneMode.Single);
+	}
+	public void LoadLevel1(){
+		SceneManager.LoadScene ("Level 1", LoadSceneMode.Single);
+	}
+}
