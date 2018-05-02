e7c3d38c41464c035e5e917c7066557e2fbc2030
ronhuff <35937652+ronhuff@users.noreply.github.com>
2018-05-02 08:30:15 -0500

    Level5 (#15)
    * Lots of art & lighting.
    
    * More lvl art, etc.
    
    * Add coins, platforms, enemies.
    
    * Commit for initial integration

---
+++ EECS448 Project 3/Assets/SpotlightRotate.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * @file - SpotlightRotate
+ * @author - Ron Huff
+ * @date - 
+ * @brief - 
+ */
+
+using UnityEngine;
+
+public class SpotlightRotate : MonoBehaviour {
+
+    #region Variables
+
+    public float MoveSpeed = 4f;
+    private bool dirRight = true;
+	#endregion
+
+	#region Unity Methods
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if(dirRight)
+        {
+            transform.Translate(Vector2.right * (MoveSpeed * Time.deltaTime));
+        }
+        else
+        {
+            transform.Translate(-Vector2.right * (MoveSpeed * Time.deltaTime));
+        }
+        if(transform.position.x >= 122f)
+        {
+            dirRight = false;
+        }
+        else if(transform.position.x <= 114f)
+        {
+            dirRight = true;
+        }
+	}
+
+	#endregion
+	
+}
